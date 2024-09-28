b6e7c87feec357569ce7337403dab055d84725d7
Brayan Alpizar <130131491+SpaceBa13@users.noreply.github.com>
2024-09-28 13:48:02 -0600

    Before Merge

---
+++ Prueba 2.pl
@@ -0,0 +1,19 @@
+acion(S0,S):- sintagma_nominal(S0,S1),
+
+sintagma_verbal(S1,S).
+
+sintagma_nominal(S0,S):- determinante(S0,S1),
+    nombre(S1,S).
+
+sintagma_verbal(S0,S):- verbo(S0,S).
+
+sintagma_verbal(S0,S):- verbo(S0,S1),
+    sintagma_nominal(S1,S).
+
+determinante([el|S],S).
+determinante([la|S],S).
+
+nombre([hombre|S],S).
+nombre([manzana|S],S).
+
+verbo([come|S],S).
