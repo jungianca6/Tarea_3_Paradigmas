16e0240efeba51aafcae87465915b7c16cc56659
Brayan Alpizar <130131491+SpaceBa13@users.noreply.github.com>
2024-09-28 17:41:27 -0600

    BUG FIXES

--- nutriTEC.pl
+++ nutriTEC.pl
@@ -11,18 +11,20 @@ inicio :-
 inicio:-inicio.%Si no se optiene la respuesta esperada, el chat no avanza.
 
 verificar_BNF(Entrada) :-
+    oracion(Entrada, []),
     write(Entrada),
-    oracion(Entrada, []), 
-    identificar_palabra([hola, como, esta], Resultados), % Llama a identificar_palabra
-    write(Resultados), nl. % Imprime los resultados (opcional)
+    indentificar_palabras_clave(Entrada).
 
 verificar_BNF(Entrada) :- 
     write('Disculpe, no he entendido.'), nl,
     inicio.
 
+indentificar_palabras_clave(Entrada):- identificar_palabra(Entrada, Resultados), % Llama a identificar_palabra,
+    write(Resultados), nl. % Imprime los resultados (opcional)
 
-%'Hola encantado de que verlo mejorar su estilo de vida. Cuénteme ¿en qué lo
-%puedo ayudar?
+%identificar_palabra([tengo, keto],X).
+%identificar_palabra([quiero, 5],X).
+
 %'Excelente iniciativa. Estamos para asesorarte en todo lo que necesites.
 %¿Tienes alguna enfermedad por la que ha iniciado este proceso?'
 %'¿Tienes pensado una cantidad específica de calorías diarias por consumir?'
